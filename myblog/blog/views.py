3e8a315af9a574cd4ef923e61caf7f2816edd01f
balaraju1278 <balaraju.mme@gmail.com>
2020-03-13 09:30:43 +0530

    added post views

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from blog.models import Post
 
-# Create your views here.
+
+def home(request):
+	context_data = dict()
+	context_data['posts'] = Post.objects.all()
+	return render(request, 'home.html', context_data)
+
+
+def create_post(request):
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		text = request.POST.get('text')
+		new_post = Post.objects.create(
+			title=title,
+			text=text,
+		)
+		return redirect('home')
+	return render(request, 'create_post.html')
