ea0984dde4b2891e97528803102596efcf133167
balaraju1278 <balaraju.mme@gmail.com>
2020-03-13 09:25:43 +0530

    added post model

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=250)
+	text = models.TextField()
+	created_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
